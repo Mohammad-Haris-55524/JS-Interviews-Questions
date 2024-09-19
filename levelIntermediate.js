ad0eca0fd07ca178a000d70e3ae195208989ae79
Mohammad-Haris-55524 <mohammadharis10678@gmail.com>
2024-09-19 23:44:49 +0500

    Added Q1(Remove Duplicacy from an array)

---
+++ levelIntermediate.js
@@ -0,0 +1,14 @@
+// -----------------------------------------------------Remove Duplicacy from an array----------------------------------------------------
+// Question # 01
+const removeDuplicates = (arr) =>{
+    let emptyArray = [];
+    // console.log(arr)
+    for (let i=0; i < arr.length; i++){
+      if(!emptyArray.includes(arr[i])){
+        emptyArray.push(arr[i]);
+      }
+    }
+    return emptyArray
+    }
+    console.log(removeDuplicates([1, 1, 2, 3, 4, 5, 4, 1]))
+    // console.log(removeDuplicates(['a', 'b', 'a', 'c', 'b']))
