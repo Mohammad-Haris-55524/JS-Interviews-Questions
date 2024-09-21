95efe7a5b898f9bd2a1e712eafb102277ecc9cfb
Mohammad-Haris-55524 <mohammadharis10678@gmail.com>
2024-09-21 21:36:07 +0500

    Added Q2 (Remove Duplicacy from an String)

--- levelIntermediate.js
+++ levelIntermediate.js
@@ -12,3 +12,17 @@ const removeDuplicates = (arr) =>{
     }
     console.log(removeDuplicates([1, 1, 2, 3, 4, 5, 4, 1]))
     // console.log(removeDuplicates(['a', 'b', 'a', 'c', 'b']))
+
+// -----------------------------------------------------Remove Duplicacy from an String----------------------------------------------------
+// Question # 02 
+const removeDuplicatesFromStrings = (str) =>{
+  let result = ''
+  console.log(str)
+  for (let i=0; i < str.length; i++){
+    if(!result.includes(str[i])){
+      result += str[i]
+    }
+  }
+  return result;
+}
+// console.log(removeDuplicatesFromStrings("Mohammad Haris"));
