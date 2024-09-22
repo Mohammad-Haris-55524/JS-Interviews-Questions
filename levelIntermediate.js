002ea2c7b9a1d2a6b096d609946c01b056d856fc
Mohammad-Haris-55524 <mohammadharis10678@gmail.com>
2024-09-22 22:16:53 +0500

    Added Q3 (Find the Longest string in a STRING)

--- levelIntermediate.js
+++ levelIntermediate.js
@@ -26,3 +26,19 @@ const removeDuplicatesFromStrings = (str) =>{
   return result;
 }
 // console.log(removeDuplicatesFromStrings("Mohammad Haris"));
+
+// Question: ****************************************Find the Longest string in a STRING **************************************************
+// Question # 03:
+let str = 'My name is Mohammad Haris'
+let strConvertedIntoArr = str.split(" ");
+console.log(strConvertedIntoArr);
+let comparisionLenght = 0;
+let result;
+for(let i=0; i < strConvertedIntoArr.length; i++){
+  if(strConvertedIntoArr[i].length > comparisionLenght){ // is.length = 2 > 4 false mohammad.lenght = 8 > 4 true
+    comparisionLenght = strConvertedIntoArr[i].length; //comparisionLenght = 8
+    result = strConvertedIntoArr[i];
+    console.log(result,comparisionLenght)
+  }
+}
+// console.log("The greatest String is: ",result);
