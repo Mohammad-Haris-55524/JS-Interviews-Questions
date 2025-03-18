78def24a3f65793b5071ac2ca262641e82a99f26
Mohammad-Haris-55524 <mohammadharis10678@gmail.com>
2025-03-18 23:17:52 +0500

    Check if a Number is Prime

--- levelEasy01.js
+++ levelEasy01.js
@@ -693,3 +693,18 @@ function arrayIntersection(arr1, arr2) {
 }
 
 console.log(arrayIntersection([1, 2, 3], [2, 3, 4])); // [2, 3]
+
+
+
+// --------------------------------------------------------- Check if a Number is Prime  ----------------------------------------------------
+function isPrime(n) {
+  if (n < 2) return false;
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) return false;
+  }
+  return true;
+}
+console.log(isPrime(7)); // true
+console.log(isPrime(10)); // false
+
+
